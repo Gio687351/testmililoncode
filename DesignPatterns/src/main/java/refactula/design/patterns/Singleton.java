1ce00c8e999931823bb2ef423e1628c2f4d1408b
Refactula <refactula@gmail.com>
2017-02-19 12:30:28 +1100

    Refactor Singleton

--- DesignPatterns/src/main/java/refactula/design/patterns/Singleton.java
+++ DesignPatterns/src/main/java/refactula/design/patterns/Singleton.java
@@ -1,9 +1,7 @@
 package refactula.design.patterns;
 
-public enum Singleton {
-    INSTANCE;
+public interface Singleton<T> {
 
-    public String tellMeTheSecret() {
-        return "The cake is a lie";
-    }
+    T getInstance();
+
 }
--- DesignPatterns/src/test/java/refactula/design/patterns/SingletonTest.java
+++ DesignPatterns/src/test/java/refactula/design/patterns/SingletonTest.java
@@ -2,13 +2,23 @@ package refactula.design.patterns;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class SingletonTest {
 
+    private final Singleton<CompanionCube> singleton = () -> CompanionCube.INSTANCE;
+
     @Test
     public void shouldAccessSingleton() {
-        assertEquals("The cake is a lie", Singleton.INSTANCE.tellMeTheSecret());
+        assertEquals("The cake is a lie", singleton.getInstance().tellMeTheSecret());
+    }
+
+    private enum CompanionCube {
+        INSTANCE;
+
+        public String tellMeTheSecret() {
+            return "The cake is a lie";
+        }
     }
 
 }
